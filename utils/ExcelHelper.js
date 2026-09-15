5231c68ed59296a8a43e27e4f8f53793a33be53f
cynthiya <cynthiya.be@gmail.com>
2026-09-15 19:37:47 -0400

    updated Excel helper

--- utils/ExcelHelper.js
+++ utils/ExcelHelper.js
@@ -1,10 +1,19 @@
-import XLSX from 'xlsx';
+import * as XLSX from 'xlsx';
+import { readFileSync } from 'node:fs';
 
 export class ExcelHelper {
-    static readExcel(filePath, sheetName){
-        const workbook = XLSX.readFile(filePath);
-        const sheet = workbook.Sheets[sheetName || workbook.SheetNames[0]];
-        return XLSX.utils.sheet_to_json<Record<string, string>>(sheet, { defval: "" });
-        
+  static readExcel(filePath, sheetName) {
+    const workbook = XLSX.read(readFileSync(filePath), {
+      type: 'buffer',
+    });
+
+    const selectedSheet = sheetName || workbook.SheetNames[0];
+    const sheet = workbook.Sheets[selectedSheet];
+
+    if (!sheet) {
+      throw new Error(`Sheet "${selectedSheet}" not found in ${filePath}`);
     }
+
+    return XLSX.utils.sheet_to_json(sheet, { defval: '' });
+  }
 }
