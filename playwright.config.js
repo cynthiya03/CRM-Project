79d9f7e0ab2d5ae69cfe71135e87deefb3a69688
cynthiya <cynthiya.be@gmail.com>
2026-09-15 19:21:21 -0400

    Base Framework

---
+++ playwright.config.js
@@ -0,0 +1,56 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+
+
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ["list"],
+    ["html" , { outputFolder: "reports/html-report", open: "never"}],
+    ["allure-playwright", {
+      outputFolder: "allure-results",
+      suiteTitle : false,
+    }],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+   screenshot : 'only-on-failure',
+    video : 'retain-on-failure',
+    trace: 'on-first-retry',
+    headless : true
+    
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    
+  ],
+
+  
+});
+
