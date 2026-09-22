330b935832bac7b4350b0bb0c47f0f0e48493cb2
cynthiya03 <108622374+cynthiya03@users.noreply.github.com>
2026-09-21 21:48:37 -0400

    Remove duplicate login step definition from More steps

--- steps/More/more_option.steps.js
+++ steps/More/more_option.steps.js
@@ -2,19 +2,14 @@ import { createBdd } from 'playwright-bdd';
 
 const { Given, When, Then } = createBdd();
 
-Given('user is logged in to CRM application', async ({}) => {
-  // Step: Given user is logged in to CRM application
-  // From: features\More\more_option.feature:7:5
-});
-
 When('user clicks on the Menu option in the menu bar', async ({}) => {
   // Step: When user clicks on the Menu option in the menu bar
-  // From: features\More\more_option.feature:8:5
+  // From: features\\More\\more_option.feature:8:5
 });
 
 Then('dropdown should display below options', async ({}, dataTable) => {
   // Step: Then dropdown should display below options
-  // From: features\More\more_option.feature:9:5
+  // From: features\\More\\more_option.feature:9:5
   const expectedOptions = dataTable.rawTable.flat().map((value) => value.trim());
   console.log('Expected menu options:', expectedOptions);
 });
